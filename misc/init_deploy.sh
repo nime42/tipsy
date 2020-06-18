37cc85dab229ee6bdf5369ba9cbdd51dc7159b3d
nime42 <nilsmeinhard@gmail.com>
2020-06-18 15:44:26 +0200

    .

---
+++ misc/init_deploy.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+REPO_URL=https://github.com/nime42/tipsy.git
+REPO_DIR=tipsy
+TARGET_DIR=~/
+git clone $REPO_URL
+cp $REPO_DIR $TARGET_DIR/
+cd $TARGET_DIR/$REPO_DIR
+npm install
+npm run initDB
