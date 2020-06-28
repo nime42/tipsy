4e2b43e576befc85dfb7b44c304772840d286004
nime42 <nils.meinhard@teliacompany.com>
2020-06-28 17:39:15 +0200

    .

--- app/utils/matchInfoHandler.js
+++ app/utils/matchInfoHandler.js
@@ -70,7 +70,7 @@ function getPlayable(product,callback) {
     getMatchDates(product, year, month, function (status, resultDates) {
         if (status) {
             let res = resultDates.find(e => {
-                return e.drawState === "Closed";
+                return e.drawState === "Open";
             });
 
 
