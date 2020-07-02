6bc04287b1f93f4542731f11c38552a1069c9d15
nime42 <nils.meinhard@teliacompany.com>
2020-07-02 13:03:23 +0200

    fixed deploy-script

--- misc/deploy.sh
+++ misc/deploy.sh
@@ -1,14 +1,15 @@
 #! /bin/sh
+WDIR=`dirname $0`
 
 REPO_URL=https://github.com/nime42/tipsy.git
-REPO_DIR=tipsy
-TARGET_DIR=~/
+REPO_DIR=$WDIR/tipsy
+TARGET_DIR=~/tipsy
 rm -rf $REPO_DIR
-git clone $REPO_URL
-rm -rf $TARGET_DIR/$REPO_DIR/public $TARGET_DIR/$REPO_DIR/app
-cp -r $REPO_DIR/public $REPO_DIR/app $REPO_DIR/package.json $TARGET_DIR/$REPO_DIR
+git clone $REPO_URL $REPO_DIR
+rm -rf $TARGET_DIR/public $TARGET_DIR/app
+cp -r $REPO_DIR/public $REPO_DIR/app $REPO_DIR/package.json $TARGET_DIR/
 rm -rf $REPO_DIR
-cd $TARGET_DIR/$REPO_DIR
+cd $TARGET_DIR/
 npm update
 npm run shutdown
 nohup npm run start 2>&1 &
--- misc/init_deploy.sh
+++ misc/init_deploy.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
+WDIR=`dirname $0`
 
 REPO_URL=https://github.com/nime42/tipsy.git
-REPO_DIR=tipsy
-TARGET_DIR=~/
+REPO_DIR=$WDIR/tipsy
+TARGET_DIR=~/tipsy
 git clone $REPO_URL
-mv $REPO_DIR $TARGET_DIR/
-cd $TARGET_DIR/$REPO_DIR
+mv $REPO_DIR $TARGET_DIR/..
+cd $TARGET_DIR/
 npm install
 npm run initDB
