8988bbc02a1e8bb0b6919329b41d66d033f85bdb
nime42 <nils.meinhard@teliacompany.com>
2020-07-16 12:35:05 +0200

    Tried to make puppeteer faster by using cachedir and skipping some files.

--- app/utils/webScraper.js
+++ app/utils/webScraper.js
@@ -1,12 +1,23 @@
 const puppeteer = require('puppeteer');
+const { resolve } = require('path');
  
 var browser=undefined;
+var page=undefined;
 
 async function getRows(url) {
     if(browser===undefined) {
-        browser = await puppeteer.launch({args: ['--no-sandbox']});
+        browser = await puppeteer.launch({args: ['--no-sandbox'],userDataDir: resolve('./tmp/puppeteer-data-dir')});
+        page = await browser.newPage();	
+        await page.setRequestInterception(true);
+        page.on('request', (request) => { 
+            if (request.resourceType() === 'stylesheet' || request.resourceType()==="image") {
+                request.abort();
+            } else {
+                request.continue();
+            }
+        });
+
     }
-  const page = await browser.newPage()	
   await page.goto(url);
   const rows=await page.evaluate(() => {
 	  let rows=""
