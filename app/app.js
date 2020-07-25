a3f38751147040c1f68690417aa7703fd84eddf6
nime42 <nils.meinhard@teliacompany.com>
2020-07-25 11:46:51 +0200

    didnt work

--- app/app.js
+++ app/app.js
@@ -58,12 +58,7 @@ app.use((req,res,next)=>{
     });
 
 
-    app.use(function (req, res, next) {
-        if ((req.get('X-Forwarded-Proto') !== 'https')) {
-            res.redirect('https://' + req.get('Host') + req.url);
-        } else
-            next();
-    });
+
 
 
 
