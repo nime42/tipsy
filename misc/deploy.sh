4d35e1e3f567f82557e4598a76f902f44c239c9e
nime42 <nilsmeinhard@gmail.com>
2020-09-16 18:39:25 +0200

    added autotag to deploy script

--- misc/deploy.sh
+++ misc/deploy.sh
@@ -6,6 +6,7 @@ REPO_DIR=$WDIR/tipsy
 TARGET_DIR=~/tipsy
 rm -rf $REPO_DIR
 git clone $REPO_URL $REPO_DIR
+$WDIR/git_autotag.sh $REPO_DIR
 rm -rf $TARGET_DIR/public $TARGET_DIR/app
 cp -r $REPO_DIR/public $REPO_DIR/app $REPO_DIR/package.json $TARGET_DIR/
 cp -r $REPO_DIR/resources/create_demo.sql $TARGET_DIR/resources
@@ -14,4 +15,6 @@ cd $TARGET_DIR/
 npm update
 npm run shutdown
 nohup npm run start 2>&1 &
+
+
 
