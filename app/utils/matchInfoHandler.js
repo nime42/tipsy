bd480d72b290b514d6fced6d0be9debd7135d249
“nilsmeinhard” <nilsmeinhard@gmail.com>
2026-06-13 17:55:34 +0200

    Refactor code formatting and improve readability in matchInfoHandler.js

--- app/utils/matchInfoHandler.js
+++ app/utils/matchInfoHandler.js
@@ -1,22 +1,23 @@
 
 
-const fetch = require('node-fetch'); 
-var config=require('../../resources/config.js');
+const fetch = require('node-fetch');
+var config = require('../../resources/config.js');
 
 
 function getMatchDates(product, year, month, callback) {
     let httpReq = config.matchInfo.url + "/draw/1/results/datepicker?product=" + product + "&year=" + year + "&month=" + month + "&_=" + new Date().getTime();
     fetch(httpReq)
-    .then(res => res.json())
-    .then(
-        json => {
-            if (json.error) {
-                callback(false, json.error);
-            } else {
-                callback(true, json.resultDates);
-            }        },
-        err => callback(false, err)
-    );
+        .then(res => res.json())
+        .then(
+            json => {
+                if (json.error) {
+                    callback(false, json.error);
+                } else {
+                    callback(true, json.resultDates);
+                }
+            },
+            err => callback(false, err)
+        );
 
 }
 
@@ -24,7 +25,7 @@ function getMatchDates(product, year, month, callback) {
 
 
 function getDraw(product, draw, callback) {
-    let httpReq = config.matchInfo.url + "/draw/1/" + product.toLowerCase().replace(" ","") + "/draws/" + draw + "?_=" + new Date().getTime();
+    let httpReq = config.matchInfo.url + "/draw/1/" + product.toLowerCase().replace(" ", "") + "/draws/" + draw + "?_=" + new Date().getTime();
     fetch(httpReq)
         .then(res => res.json())
         .then(
@@ -37,35 +38,35 @@ function getDraw(product, draw, callback) {
 
 
 function getDrawAndResult(product, draw, callback) {
-    let urls=[];
-    urls.push("/draw/1/" + product.toLowerCase().replace(" ","") + "/draws/" + draw);
-    urls.push("/draw/1/" + product.toLowerCase().replace(" ","") + "/draws/forecast/" + draw);
-    urls.push("/draw/1/" + product.toLowerCase().replace(" ","") + "/draws/" + draw+"/result");
-    let httpReq=config.matchInfo.url+"/multifetch?urls="+urls.join("|")+"&_="+ new Date().getTime();
+    let urls = [];
+    urls.push("/draw/1/" + product.toLowerCase().replace(" ", "") + "/draws/" + draw);
+    urls.push("/draw/1/" + product.toLowerCase().replace(" ", "") + "/draws/forecast/" + draw);
+    urls.push("/draw/1/" + product.toLowerCase().replace(" ", "") + "/draws/" + draw + "/result");
+    let httpReq = config.matchInfo.url + "/multifetch?urls=" + urls.join("|") + "&_=" + new Date().getTime();
     fetch(httpReq)
         .then(res => res.json())
         .then(
             json => {
-                let res={};
-                res.draws=parseDraw(json.responses[0]);
-                res.forecast=parseForecast(json.responses[1]);
-                res.result=parseResult(json.responses[2]);
+                let res = {};
+                res.draws = parseDraw(json.responses[0]);
+                res.forecast = parseForecast(json.responses[1]);
+                res.result = parseResult(json.responses[2]);
 
-                res.draws.draws.filter(d=>(d.result==="? - ?")).forEach(d=>{
-                    let i=d.eventNumber-1;
-                    let matchInfo=(res.forecast!=null && res.forecast.matchinfo)?res.forecast.matchInfo[i]:undefined
-                    if(matchInfo) {
-                        let home=matchInfo.home;
-                        let away=matchInfo.away;
-                        d.result=`${home?home:"0"} - ${away?away:"0"}`
+                res.draws.draws.filter(d => (d.result === "? - ?")).forEach(d => {
+                    let i = d.eventNumber - 1;
+                    let matchInfo = (res.forecast != null && res.forecast.matchinfo) ? res.forecast.matchInfo[i] : undefined
+                    if (matchInfo) {
+                        let home = matchInfo.home;
+                        let away = matchInfo.away;
+                        d.result = `${home ? home : "0"} - ${away ? away : "0"}`
                     } else {
-                        d.result="0 - 0";
+                        d.result = "0 - 0";
                     }
                 });
 
 
-                callback(true,res);
-                
+                callback(true, res);
+
             },
             err => callback(false, err)
         );
@@ -73,23 +74,23 @@ function getDrawAndResult(product, draw, callback) {
 
 //getDrawAndResult('topptipsetextra',1275,function(status,data) {console.log(status,data)});
 
-function getDrawAndResultCache(draws, callback=console.log) {
-    let nrofCalls=draws.length;
-    let cache={};
-    if(nrofCalls===0) {
+function getDrawAndResultCache(draws, callback = console.log) {
+    let nrofCalls = draws.length;
+    let cache = {};
+    if (nrofCalls === 0) {
         callback(cache);
         return;
     };
-    draws.forEach(d=>{
-        let product=d.product;
-        let drawNumber=d.drawnumber;
-        getDrawAndResult(product,drawNumber, function(status,response) {
+    draws.forEach(d => {
+        let product = d.product;
+        let drawNumber = d.drawnumber;
+        getDrawAndResult(product, drawNumber, function (status, response) {
             nrofCalls--;
-            cache[product+";"+drawNumber]={status:status,response:response};
-            if(nrofCalls===0) {
+            cache[product + ";" + drawNumber] = { status: status, response: response };
+            if (nrofCalls === 0) {
                 callback(cache);
             }
-            
+
         });
 
 
@@ -104,17 +105,17 @@ getDrawAndResultCache(draws);
 //getDraw2("europatipset",1966,function(status,data) {console.log(status,data)});
 */
 
-function getPlayable(product,callback) {
+function getPlayable(product, callback) {
     let date = new Date();
     let year = date.getFullYear();
     let month = date.getMonth() + 1;
     //console.log(year,month);
     getMatchDates(product, year, month, function (status, resultDates) {
         if (status) {
-            let drawDates=resultDates.filter(e=>(e.drawState === "Open"));
-            getMultipleDraws(drawDates,function(draws) {
-                if(draws.length===0) {
-                    callback(false,"NOT_PLAYABLE");
+            let drawDates = resultDates.filter(e => (e.drawState === "Open"));
+            getMultipleDraws(drawDates, function (draws) {
+                if (draws.length === 0) {
+                    callback(false, "NOT_PLAYABLE");
                 } else {
                     callback(true, draws);
                 }
@@ -159,30 +160,30 @@ function getMultipleDraws(draws, callback) {
 
 function parseResult(data) {
     let r = data.result;
-    if(!r) {
+    if (!r) {
         return null;
     }
 
-    let res={};
-    res.cancelled=r.cancelled;
+    let res = {};
+    res.cancelled = r.cancelled;
     res.productName = r.productName;
-    res.productId=r.productId;
-    res.distribution=r.distribution;
-    res.drawNumber=r.drawNumber;
-    res.regCloseTime=r.regCloseTime;
-    res.results=[];
+    res.productId = r.productId;
+    res.distribution = r.distribution;
+    res.drawNumber = r.drawNumber;
+    res.regCloseTime = r.regCloseTime;
+    res.results = [];
     r.events.forEach(e => {
-        let row={};
-        row.eventDescription=e.eventDescription;
-        row.cancelled=e.cancelled;
-        row.eventNumber=e.eventNumber;
-        row.outcome=e.outcome;
-        if(row.cancelled && row.outcome) {
-            row.result="Lottad "+row.outcome;
+        let row = {};
+        row.eventDescription = e.eventDescription;
+        row.cancelled = e.cancelled;
+        row.eventNumber = e.eventNumber;
+        row.outcome = e.outcome;
+        if (row.cancelled && row.outcome) {
+            row.result = "Lottad " + row.outcome;
         } else {
-            row.result=e.outcomeScore.home+" - "+e.outcomeScore.away;
+            row.result = e.outcomeScore.home + " - " + e.outcomeScore.away;
         }
-        row.status="Avslutad"; // To harmonze with drawInfo (see parseDraw)
+        row.status = "Avslutad"; // To harmonze with drawInfo (see parseDraw)
         res.results.push(row);
 
     });
@@ -190,18 +191,18 @@ function parseResult(data) {
 }
 
 function parseForecast(data) {
-    
+
     let r = data.forecastResult;
 
-    if(!r) {
+    if (!r) {
         return null;
     }
     //console.log(r.drawResults);
-    let matchInfo=r.drawResults.map(e=>{return {home:e.home,away:e.away,outcome:e.outcome,time:e.time}});
-    let res={};
-    if(r.winresult) {
-        res.winresult=r.winresult.map(e=>{e.amount=e.winValue;return e});
-        res.matchInfo=matchInfo;
+    let matchInfo = r.drawResults.map(e => { return { home: e.home, away: e.away, outcome: e.outcome, time: e.time } });
+    let res = {};
+    if (r.winresult) {
+        res.winresult = r.winresult.map(e => { e.amount = e.winValue; return e });
+        res.matchInfo = matchInfo;
         return res;
 
     } else {
@@ -209,39 +210,39 @@ function parseForecast(data) {
     }
 }
 
-function parseDraw(data) {                
+function parseDraw(data) {
     let r = data.draw;
 
-    if(!r) {
+    if (!r) {
         return null;
     }
 
-    let res={};
-    res.drawState=r.drawState;
+    let res = {};
+    res.drawState = r.drawState;
     res.productName = r.productName; //r.productFamily?r.productFamily:r.productName; //handle when productName="topptipset Extra for example"
-    res.productId=r.productId;
-    res.drawNumber=r.drawNumber;
-    res.regCloseTime=r.regCloseTime;
-    res.rowPrice=r.rowPrice.replace(",",".");
-    res.draws=[];
+    res.productId = r.productId;
+    res.drawNumber = r.drawNumber;
+    res.regCloseTime = r.regCloseTime;
+    res.rowPrice = r.rowPrice.replace(",", ".");
+    res.draws = [];
     r.drawEvents.forEach(e => {
-        let row={};
-        row.eventDescription=e.eventDescription;
-        row.eventNumber=e.eventNumber;
-        row.odds=e.odds;
-        row.svenskaFolket=e.svenskaFolket;
-        row.match=e.match;
-        row.matchTime=getMatchTime(row.match);
-        row.status=e.match.status;//To harmonize with resultInfo (see parseResult)
-        let current=e.match.result.find(e=>(e.description==="Full time" ));
-        if(current) {
-            row.result=current.home+" - "+current.away;//Also for harmonizing with resultInfo
+        let row = {};
+        row.eventDescription = e.eventDescription;
+        row.eventNumber = e.eventNumber;
+        row.odds = e.odds;
+        row.svenskaFolket = e.svenskaFolket;
+        row.match = e.match;
+        row.matchTime = getMatchTime(row.match);
+        row.status = e.match.status;//To harmonize with resultInfo (see parseResult)
+        let current = e.match.result.find(e => (e.description === "Full time"));
+        if (current) {
+            row.result = current.home + " - " + current.away;//Also for harmonizing with resultInfo
         } else {
-            current=e.match.result.find(e=>(e.description.match(/.*Current/i)));
-            if(current) {
-                row.result=current.home+" - "+current.away;//Also for harmonizing with resultInfo
+            current = e.match.result.find(e => (e.description.match(/.*Current/i)));
+            if (current) {
+                row.result = current.home + " - " + current.away;//Also for harmonizing with resultInfo
             } else {
-             row.result="? - ?";
+                row.result = "? - ?";
             }
         }
 
@@ -253,35 +254,35 @@ function parseDraw(data) {
 }
 
 function getMatchTime(match) {
-    if(match.status.match(/Halvtid/i)) {
+    if (match.status.match(/Halvtid/i)) {
         return "HT";
     }
 
-    if(match.status.match(/halvlek/i)) {
-        let d=new Date(match.statusTime);
-        let now=new Date();
-        let minutes=Math.ceil((now-d)/(1000*60));
-        
-        if(minutes>45) {
-            minutes=45;
+    if (match.status.match(/halvlek/i)) {
+        let d = new Date(match.statusTime);
+        let now = new Date();
+        let minutes = Math.ceil((now - d) / (1000 * 60));
+
+        if (minutes > 45) {
+            minutes = 45;
         }
-        if(match.status.match(/andra halvlek/i)) {
-            minutes+=45;
+        if (match.status.match(/andra halvlek/i)) {
+            minutes += 45;
         }
-        return minutes+"'";
+        return minutes + "'";
 
     } else {
         return null;
     }
-    
+
 }
 
-module.exports={
-    getPlayable:getPlayable,
-    getDrawAndResult:getDrawAndResult,
-    getDrawAndResultCache:getDrawAndResultCache,
-    getMatchDates:getMatchDates,
-    getDraw:getDraw
+module.exports = {
+    getPlayable: getPlayable,
+    getDrawAndResult: getDrawAndResult,
+    getDrawAndResultCache: getDrawAndResultCache,
+    getMatchDates: getMatchDates,
+    getDraw: getDraw
 }
 
 
